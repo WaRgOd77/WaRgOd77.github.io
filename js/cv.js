42e7f26d4a826c609014930de6775e2f2990c5fa
WaRgOd77 <gutierrezdiegoarg@gmail.com>
2023-02-27 20:05:59 -0300

    correccion2

--- js/cv.js
+++ js/cv.js
@@ -2,7 +2,7 @@
 function tabla1(){
     atrás()
     document.getElementById("datospers").style.visibility = "visible";
-    document.getElementById("soni1").play();
+   
     
     
 }
@@ -10,19 +10,19 @@ function tabla1(){
 function tabla2(){
     atrás()
     document.getElementById("estudios").style.visibility = "visible";
-    document.getElementById("soni2").play();
+    
 }
     
 function tabla3(){
     atrás()
     document.getElementById("trabajos").style.visibility = "visible";
-    document.getElementById("soni3").play();
+    
 }
  
 function atrás(){
     document.getElementById('datospers').style.visibility = 'collapse';
     document.getElementById('estudios').style.visibility = 'collapse';
     document.getElementById('trabajos').style.visibility = 'collapse';
-    //document.getElementById("soni").play();
+    
 }
 
